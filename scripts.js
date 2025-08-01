4e4a3beb21cd7aa9f9661b6a8879931da95764f9
mjoviedo <moviedo@isep-cba.edu.ar>
2025-08-01 12:40:05 -0300

    Testea boton de fullscreen

--- scripts.js
+++ scripts.js
@@ -47,3 +47,31 @@ function multimediaFoco(entries){
 audioscroll.forEach(audioscroll => { 
     observer.observe(audioscroll);
 });
+
+
+
+
+/* Get the documentElement (<html>) to display the page in fullscreen */
+var elem = document.documentElement;
+
+/* View in fullscreen */
+function openFullscreen() {
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.webkitRequestFullscreen) { /* Safari */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE11 */
+    elem.msRequestFullscreen();
+  }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) { /* Safari */
+    document.webkitExitFullscreen();
+  } else if (document.msExitFullscreen) { /* IE11 */
+    document.msExitFullscreen();
+  }
+}
