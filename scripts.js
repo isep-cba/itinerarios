9a5b3043d588c6051dd1b27a595aafd3788c2fee
moviedo <moviedo@isep-cba.edu.ar>
2025-09-11 17:29:00 -0300

    Agrega seccion mapa

--- scripts.js
+++ scripts.js
@@ -13,6 +13,26 @@ function openFullscreen() {
   }
 }
 
+/* MAPA DE CORDOBA */
+
+/* PRUEBAS MAPA */
+
+var map = L.map('map', {
+    crs: L.CRS.Simple,
+    minZoom: -1
+});
+
+
+var bounds = [[0,0], [1551,1142]];
+var image = L.imageOverlay('img/mapa-cordoba.jpg', bounds).addTo(map);
+
+map.fitBounds(bounds);
+
+
+var marker = L.marker([998, 485]).addTo(map);
+marker.bindPopup("    <img src='img/mapa-cordoba.jpg' style='width: 100%;'><b>Escuela Normal Alejandro Carbó</b><br>Av. Colón 2000. Aquí fue la primera escuela normal, donde dio clases Jennie Howard.").openPopup();
+
+
 /* NAVBAR  hide/show on scroll 
 
 var prevScrollpos = window.pageYOffset;
