115d254cc7bcb72db5605d0e933ed762419b7fcf
moviedo <moviedo@isep-cba.edu.ar>
2025-09-12 19:28:07 -0300

    Agrega seccion 360 interna

--- scripts.js
+++ scripts.js
@@ -47,3 +47,45 @@ window.onscroll = function() {
 }
 
 */
+
+
+
+// Pruebas de audio automatico con scroll
+const body = document.getElementById(".body");
+const audioscroll = document.querySelectorAll(".audioscroll");
+
+
+const options = {
+    root: null,
+    rootMargin: "-40px",
+    threshold: 0.6,
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+    entries.forEach(entry => {
+        console.log(entry);
+        if(entry.isIntersecting){
+            //elemento visible
+            entry.target.play();
+        }else{
+            entry.target.pause();
+        }
+    });
+}, options);
+
+
+
+function multimediaFoco(entries){
+    let entry = entries[0];
+    if(entry.isIntersecting){
+        //elemento visible
+        audioscroll.play();
+    }
+    else{
+        audioscroll.pause();
+    }
+}
+
+audioscroll.forEach(audioscroll => { 
+    observer.observe(audioscroll);
+});
