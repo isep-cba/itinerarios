3f1f740ceabf1f04ca263b153bd570083fada6f3
moviedo <moviedo@isep-cba.edu.ar>
2025-09-15 19:19:09 -0300

    Afina coordenadas de escuelas en el mapa

--- map.js
+++ map.js
@@ -5,7 +5,8 @@ documentacion https://leafletjs.com/reference.html
 
 var map = L.map('map', {
     crs: L.CRS.Simple,
-    minZoom: -5
+    minZoom: -2,
+    maxZoom: 0.03
 });
 
 
@@ -27,23 +28,23 @@ var pin = L.icon({
 var cruzdeleje = L.marker([3700, 3080],{icon: pin}).addTo(map);
 cruzdeleje.bindPopup("<h1>1917</h1><br><em>Escuela Normal Superior República del Perú</em><br>Cruz del Eje");
 
-var sanfrancisco = L.marker([2730, 6380],{icon: pin}).addTo(map);
+var sanfrancisco = L.marker([2760, 6380],{icon: pin}).addTo(map);
 sanfrancisco.bindPopup("<h1>1912</h1><br><em>Escuela Normal Nacional</em><br>San Francisco");
 
-var alberdi = L.marker([2730, 3380],{icon: pin}).addTo(map);
+var alberdi = L.marker([2760, 3710],{icon: pin}).addTo(map);
 alberdi.bindPopup("<h1>1906</h1><br><em>Escuela Graduada Superior Normal Provincial de Niñas Juan Bautista Alberdi</em><br>Córdoba Capital");
 
-var maestras = L.marker([2530, 6380],{icon: pin}).addTo(map);
+var maestras = L.marker([2820, 3810],{icon: pin}).addTo(map);
 maestras.bindPopup("<h1>1884</h1><br><em>Escuela Normal Nacional de Maestras </em><br>Córdoba Capital");
 
-var olmos = L.marker([2530, 6380],{icon: pin}).addTo(map);
+var olmos = L.marker([2730, 3910],{icon: pin}).addTo(map);
 olmos.bindPopup("<h1>1909</h1><br><em>Escuela Normal Provincial para Varones Jose Vicente de Olmos </em><br>Córdoba Capital");
 
-var riocuarto = L.marker([1530, 5380],{icon: pin}).addTo(map);
+var riocuarto = L.marker([500, 3680],{icon: pin}).addTo(map);
 riocuarto.bindPopup("<h1>1909</h1><br><em>Escuela Normal Nacional Mixta</em><br>Río Cuarto");
 
-var bellville = L.marker([1530, 5380],{icon: pin}).addTo(map);
+var bellville = L.marker([1200, 5650],{icon: pin}).addTo(map);
 bellville.bindPopup("<h1>1909</h1><br><em>Escuela Normal Nacional Mixta</em><br>Bell Ville");
 
-var villadolores = L.marker([2000, 2380],{icon: pin}).addTo(map);
-villadolores.bindPopup("<h1>1910</h1><br><em>Escuela Normal Rural Mixta</em><br>Bell Ville");
+var villadolores = L.marker([2100, 2600],{icon: pin}).addTo(map);
+villadolores.bindPopup("<h1>1910</h1><br><em>Escuela Normal Rural Mixta</em><br>Villa Dolores");
